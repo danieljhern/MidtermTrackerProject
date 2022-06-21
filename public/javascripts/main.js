79daa3ead4566171843e0881747a881587771594
Daniel Hernandez <daherna@microsoft.com>
2022-06-20 20:26:38 -0700

    updating for final project

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,57 +1,69 @@
-
+//ARRAYS FOR PRESET CANDIDATES AND THE USER'S ARRAY
 let clientCandidateArray = []
 let clientUserArray = []
 
-let candidateObject = function (cName, cNotes, cID, cParty) {
+//CANDIDATE OBJECT CONSTRUCTOR
+let candidateObject = function (cName, cNotes, cID, cParty, cDistrict, cSite) {
     this.testName = cName;
     this.testNotes = cNotes;
     this.testID = cID;
     this.testParty = cParty
+    this.testDistrict =cDistrict
+    this.testSite = cSite
 }
 
 
 
+//LOADING EVENT LISTENERS AND FUNCTIONS FOR PAGES
+$(document).on("pagebeforeshow","#districts",function(){
+    $.get('/getDistrictCandidates', function (data, status) {
+        clientCandidateArray = data;
+        document.getElementById('select-district').addEventListener('click', getSelectedValue);
+        document.getElementById('myBtn').addEventListener('click', addToList);
+    
+})});
+
+$(document).on("pagebeforeshow","#add-candidate",function(){
+    document.getElementById("custom-add").addEventListener('click', customCandidateAdd);
+    document.getElementById("buttonClear").addEventListener("click", clearCustomData); 
+        });
+
+$(document).on("pagebeforeshow","#list", (myLIst));
 
 
+//FUNCTION TO GRAB SELECTED DISTRICT
 function getSelectedValue(){
-    $.get("/getDistrictCandidates", function (data, status) {
-        clientCandidateArray = data
-    });
-
 
     var selectedValue = document.getElementById('select-district').value;
     document.getElementById('display-current-district').innerHTML=('District ' + selectedValue + " candidates are:");
-    outputCandidateCheckbox();
 
-}
+    var selectedValue = document.getElementById('select-district').value;
+    var myul = document.getElementById("myList");
+    myul.innerHTML = '';
 
-function outputCandidateCheckbox() {
-var selectedValue = document.getElementById('select-district').value;
-var myul = document.getElementById("myList");
-myul.innerHTML = '';
-
-//Creates the list items for Candidates by District for the user to pick
-
-
-for(sv in clientCandidateArray) {
-    if (selectedValue == clientCandidateArray[sv].district){
+    //Creates the list items for Candidates by District for the user to pick
+    for(sv in clientCandidateArray) {
+        if (selectedValue == clientCandidateArray[sv].district){
     
     
-            cname = clientCandidateArray[sv].name;
+            cName = clientCandidateArray[sv].name;
             cParty = clientCandidateArray[sv].party;
+            cDistrict = clientCandidateArray[sv].district
+            cSite = clientCandidateArray[sv].site
             candidateId = "D"+selectedValue+"-" + clientCandidateArray[sv].name;
-            candidateInfo = clientCandidateArray[sv].name + "     " + "Party Affiliation:  " + clientCandidateArray[sv].party;
             //Creating input elements
             var li = document.createElement('input');
             li.type = 'checkbox';
             li.id = candidateId;
             li.value = clientCandidateArray[sv].name
             li.name = "checkbox-item"
+            li.innerHTML = cSite
             //creating label elements
-            var liLabel = document.createElement('label')
+            var liLabel = document.createElement('li')//*(label)
             liLabel.htmlFor = candidateId;
-            liLabel.appendChild(document.createTextNode('Name: ' + cname + ', Party: ' + cParty));
-    
+            liLabel.id = cSite
+            liLabel.appendChild(document.createTextNode('Name: ' + cName + ',' +'\xa0\xa0'+ 'Party: ' + cParty + ',' + '\xa0\xa0' + 'Website: '+ cSite))
+   
     
             var br = document.createElement('br');
             myul.appendChild(liLabel);
@@ -63,48 +75,35 @@ for(sv in clientCandidateArray) {
 }};
 
 
-const element = document.getElementById("myBtn");
+//const element = document.getElementById("myBtn");
 
 
 
 
 
-// Allows user to add Candidates from selected checkboxes
+//FUNCTION  TO ADD SELECTED CANDIDATES TO LIST
 function addToList() {
-    demo = document.getElementById("demo");
-
-
-    $.get("/getCandidates", function (data, status) {
-        clientUserArray = data
-    });
-
-
-    let checkboxes = document.querySelectorAll('input[name="checkbox-item"]:checked');
-
- 
-
-//check selected checkboxes to see if they are in the "clientIncludesArray" and pushes new object to 
-//"userArray" if not
-    checkboxes.forEach((checkbox) => {        
+    $.get('/getCandidates', function (data, status) {
+        clientUserArray = data;
+        console.log(clientUserArray);
+        demo = document.getElementById("demo");
+        let checkboxes = document.querySelectorAll('input[name="checkbox-item"]:checked');
+        checkboxes.forEach((checkbox) => {        
 
         const check = clientUserArray.some(e => e.testID === checkbox.id);
-    //var duplicate = clienUserArray.includes(checkbox.id);
-        //if (duplicate == false) {
+
         if (check == false) {
             tCandidate = "Added - "+ checkbox.parentElement.textContent;
             tNotes = "";
             var tID = checkbox.id;
-            var tVal = checkbox.value
+            var tVal = checkbox.value;
+            var tSite = checkbox.parentElement.id
             var list = document.createElement('li');
             list.id = tCandidate;
-            list.innerHTML = tCandidate;
             tParty = tID.charAt(0)
+            tDistrict = tID.charAt(1)
 
-            demo.appendChild(list);
-    
-    //add back tCandidate if name doesnt work
-         //userArray.push(new candidateObject(tVal, tNotes, tID, tParty));
-            let newCandidate = (new candidateObject(tVal, tNotes, tID, tParty));
+            let newCandidate = (new candidateObject(tVal, tNotes, tID, tParty, tDistrict, tSite));
             $.ajax({
                 url: '/addCandidate',
                 type: "POST",
@@ -113,52 +112,42 @@ function addToList() {
                 dataType: "json",
                 success: function (result) {
                     console.log(result);
-                    //document.location.href = "index.html#ListAll"
-                }
-            });
-            $.get("/getCandidates", function (data, status) {
-                clientUserArray = data
-            });
-        
+                },
+                error: function(xhr, textStatus, errorThrown){
+                    console.log(textStatus)
+            }});
+            alert(tVal + " has been added");
 
  
     }
     else {alert(checkbox.value + " " + "is already in your list")}
 
-   });
+   })
+
+});
 
 }
 
 
 
-document.addEventListener('DOMContentLoaded', () => {
     
-    //not sure if this is best for DOMContentLoaded
 
-    $(document).on("pagebeforeshow","#list",function(){
-        
-    //  });
+//FUNCTION TO LIST OUT USERS ARRAY WITH BUTTONS TO ADD NOTES AND REMOVE OBJECT
+ function myLIst(){
+    $.get('/getCandidates', function (data, status) {
+        clientUserArray = data;
+
 
     var myCandidateList = document.getElementById('myCandidates');
-    let btn = document.getElementById('btn');
+    var theList = document.getElementById("myCandidates");
+    theList.innerHTML = "";
+
     
-  //Diplays list from User Array with buttons to add notes or remove candidates from array  
-  // btn.addEventListener('click', ()=>
-    //{
-
-
-        var theList = document.getElementById("myCandidates");
-        theList.innerHTML = "";
-
-        $.get("/getCandidates", function (data, status) {
-            clientUserArray = data
-        });
-    
-
+    //builds li items with buttons for adding notes and deleting candidates
     clientUserArray.forEach(function (element, i) {
     var listItem = document.createElement('li');
     listItem.id = element.testID + "-li-id";
-    listItem.innerHTML = element.testName  + "     Party: " + element.testParty;
+    listItem.innerHTML = element.testName + ",\xa0\xa0Party: " + element.testParty  + ",\xa0\xa0Site: " + element.testSite ;
     //text fields, will play around with bigger text areas later
     var InputItem = document.createElement("input");
     InputItem.id = "button-for-"+element.testID+"-notes";
@@ -192,19 +181,21 @@ document.addEventListener('DOMContentLoaded', () => {
     dbuttonid = dltBtn.id;
     document.getElementById(buttonid).addEventListener('click', addNote);
     document.getElementById(dbuttonid).addEventListener('click', removeCandidate);
-    })//})
-});
+    })})}; 
+
     
-
-    //ADDS CUSTOM CANDIDATE TO USER ARRAY
-    document.getElementById("buttonAdd1").addEventListener("click", function () {
+    
+//ADDS CUSTOM CANDIDATE TO USER ARRAY
+function customCandidateAdd() {
        
-
+    $.get('/getCandidates', function (data, status) {
+        clientUserArray = data
 
         cCandName = document.getElementById("custom-candidate-name").value;
         cCandNotes = document.getElementById("custom-candidate-notes").value;
         selectedParty = document.getElementById("select-party").value;
-        cCandDistrict = document.getElementById("custom-candidate-district").value
+        cCandDistrict = document.getElementById("add-select-district").value;
+        cCandSite = document.getElementById("custom-candidate-site").value;
         
 
         if (selectedParty.value = "Democrat"){
@@ -220,7 +211,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         cCandID = cCandParty + cCandDistrict + "-"+cCandName;
 
-        let newCandidate = (new candidateObject(cCandName,cCandNotes,cCandID,cCandParty));
+        let newCandidate = (new candidateObject(cCandName,cCandNotes,cCandID,cCandParty, cCandDistrict, cCandSite));
         $.ajax({
             url: '/addCandidate',
             type: "POST",
@@ -228,129 +219,122 @@ document.addEventListener('DOMContentLoaded', () => {
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             success: function (result) {
-                console.log(result);
+                console.log(result)
+                },
+                error: function(xhr, textStatus, errorThrown){
+                    console.log(textStatus)
+                
             }
         });
 
-        alert(cCandName + " has been added"); 
+        alert(cCandName + " has been added");
+        clearCustomData(); 
+        });
 
         
 
         
-     });
+     };
      
-     document.getElementById("buttonClear").addEventListener("click", function () {
+//CLEARS THE DATA FIELDS FOR CUSTOM ADD
+function clearCustomData()     
+     {
          document.getElementById("custom-candidate-name").value = "";
          document.getElementById("custom-candidate-notes").value = "";
-     });
-
-})
+         document.getElementById("custom-candidate-district").value = "";
+     };
 
 //ADDNOTE FUNCTION 
-    function addNote(){
-
-$.get("/getCandidates", function (data, status) {
-            clientUserArray = data
-        });
-        bID = this.id;
-        slicebID = bID.slice(11);
-        textboxNotes = document.getElementById(bID+"-notes").value;
-
-        for (y in clientUserArray){
-
- 
-            if (slicebID == clientUserArray[y].testID){
-
-                clientUserArray[y].testNotes = textboxNotes;
-
-
-                passID = clientUserArray[y].testID;
-                repNotes = clientUserArray[y].testNotes;
-                repName = clientUserArray[y].testName;
-                repID = clientUserArray[y].testID;
-                repParty = clientUserArray[y].testParty;
-
-
-                let updatedCandidate = (new candidateObject(repName, repNotes, repID, repParty))
+function addNote(){
 
    
+
+        bID = this.id;
+        slicebID = bID.slice(11);
+        let textboxNotes = document.getElementById(this.id + "-notes").value;
+        $.get('/getCandidates', function (data, status) {
+            clientUserArray = data
+      
+
+        for (y in clientUserArray){ 
+            if (slicebID == clientUserArray[y].testID){
+
+                clientUserArray[y].testNotes = textboxNotes    
+                passID = clientUserArray[y].testID
+                repNotes = clientUserArray[y].testNotes
+                repName = clientUserArray[y].testName
+                repID = clientUserArray[y].testID
+                repParty = clientUserArray[y].testParty
+                repDistrict = clientUserArray[y].testDistrict
+                repSite = clientUserArray[y].testSite
+
+
+
+                let updatedCandidate = (new candidateObject(repName, repNotes, repID, repParty, repDistrict, repSite))
+   
                     $.ajax({
-                        url : "/UpdateCandidate/"+ updatedCandidate.testID,
+                        url : "/updateCandidate/"+ updatedCandidate.testID,
                         type: "PUT",
                         data: JSON.stringify(updatedCandidate),
                         contentType: "application/json; charset=utf-8",
+
+                        success: function(result){ 
+                            console.log(result);
+                            myLIst();     
+    
+                                },
+                        error:    function (xhr, textStatus, errorThrown) {
+                            console.log("unable to add note: "+ result)
+                             }   
                             
                     
                     });
-                                          
+    
+                }
+        }       });
 
-                    
-                
-
-                alert("'Add Notes' is currently down");
-        
-
-
-            }
-        }
-
-        $.get("/getCandidates", function (data, status) {
-            clientUserArray = data
-        });
-
-
-
-
-
-
-    }
+    };
 
 //REMOVE CANDIDATE FUNCTION 
-
     function removeCandidate(){
-        rID = this.id
-        slicerID = rID.slice(18)
-        
-        const parent = document.getElementById("myCandidates")
-        var listID=document.getElementById(slicerID + "-li-id")        
-        var btnID = document.getElementById("button-for-"+slicerID)
-
-
-        var dbtnid = document.getElementById(this.id)
-        var txtID = document.getElementById("button-for-"+slicerID+"-notes")
-
-        $.get("/getCandidates", function (data, status) {
-            clientUserArray = data
-        });
         
 
-    
+        console.log("before remove: " + clientUserArray);
+  
+
+        rID = this.id;
+        slicerID = rID.slice(18);
+        
+        const parent = document.getElementById("myCandidates");
+        var listID=document.getElementById(slicerID + "-li-id");        
+        var btnID = document.getElementById("button-for-"+slicerID);
 
 
-      
-
-        for (p in clientUserArray){
-            if (slicerID == clientUserArray[p].testID){
-
-                passID = clientUserArray[p].testID;
- 
-                //const index = clientUserArray.indexOf(clientUserArray[p]);
-                //userArray.splice(index, 1);
-                parent.removeChild(listID);
-                parent.removeChild(btnID);
-                parent.removeChild(txtID);
-                parent.removeChild(dbtnid);
+        var dbtnid = document.getElementById(this.id);
+        var txtID = document.getElementById("button-for-"+slicerID+"-notes"); 
+        $.get('/getCandidates', function (data, status) {
+            clientUserArray = data;                 
+            console.log("delete called with pass: " + slicerID);
 
                 $.ajax({
                     type: "DELETE",
-                    url: '/deleteCandidate/' + passID,
-                    success: function(result){
-                        console.log(result)
-                    },
-                });
+                    url: '/deleteCandidate/' + slicerID,
+                    
+                    success: function(result){ 
+                        console.log(result);
+                        parent.removeChild(listID);
+                        parent.removeChild(btnID);
+                        parent.removeChild(txtID);
+                        parent.removeChild(dbtnid);           
 
-            }
-
+                            },      
+                    error: function (xhr, textStatus, errorThrown) {
+                                console.log("unable to delete: " + slicerID)
+                                 }
+                                  
+                    }); 
+                
+                
+            })
+        
         }
-      
-    }
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,123 @@
 var express = require('express');
 var router = express.Router();
 
-testCandidateArray = [
-    { district: 1, name: "Davina Duerr", party: "D" },
-    { district: 1, name: "Shelley Kloba", party: "D" },
-    { district: 1, name: "John Peeples", party: "R" },
-    { district: 1, name: "Jerry Zeiger-Buccola", party: "R" },
-    { district: 2, name: "Andrew Barkis", party: "R" },
-    { district: 2, name: "JT Wilcox", party: "R" },
-    { district: 3, name: "Marcus Riccelli", party: "D" },
-    { district: 3, name: "Timm Ormsby ", party: "D" },
-    { district: 3, name: "Scotty Nicol", party: "R" },
-    { district: 3, name: "Natalie Poulson", party: "R" },
-    { district: 4, name: "Ted Cummings", party: "D" },
-    { district: 4, name: "MJ Bolt", party: "R" },
-    { district: 3, name: "Suzanne Schmidt", party: "R" },
-    { district: 4, name: "Rob Chase ", party: "R" },
-    { district: 4, name: "Leonard Christian", party: "R" },
+candidateArray = [
+    { district: 1, name: "Suzan DelBene", party: "D", site: "https://delbene.house.gov/" },
+    { district: 1, name: "Vincent Cavaleri", party: "R", site: "https://www.cavaleriforcongress.com/" },
+    { district: 1, name: "Derek Chartrand", party: "R", site: "https://www.chartrandforcongress.com/"},
+    { district: 1, name: "Matthew Heines", party: "R", site: "https://ballotpedia.org/Matthew_Heines"},
+    { district: 2, name: "RickLarsen", party: "D", site: "https://larsen.house.gov/"},
+    { district: 2, name: "JasonCall", party: "D", site: "https://www.callforcongress.com/"},
+    { district: 2, name: "CodyHart", party: "R", site: "https://codyhart.org/"},
+    { district: 2, name: "LeifJohnson", party: "R", site: "https://www.leifjohnsonforcongress.com/"},
+    { district: 2, name: "CarrieKennedy", party: "R", site: "http://www.carriekennedy2022.com/"},
+    { district: 2, name: "DanMatthews", party: "R", site: "https://ballotpedia.org/Dan_Mathews"},
+    { district: 2, name: "BrandonStalnaker", party: "R", site: "https://vote-usa.org/Intro.aspx?State=WA&Id=WAStalnakerBrandonLee"},
+    { district: 2, name: "BillWheeler", party: "R", site: "https://ballotpedia.org/Bill_Wheeler"},
 
+
+    { district: 3, name: "Jaime Herrera Beutler", party: "R", site: "https://jhb.house.gov/"},
+    { district: 3, name: "Leslie French", party: "R", site: "http://frenchforcongress.org/"},
+    { district: 3, name: "Marie Gluesenkamp Perez", party: "D", site: "https://marieforcongress.com/"},
+    { district: 3, name: "Chris Jenkins", party: "D", site: "https://ballotpedia.org/Chris_Jenkins"},
+    { district: 3, name: "Joe Kent", party: "R", site: "https://joekentforcongress.com/"},
+    { district: 3, name: "Vicki Kraft", party: "R", site: "https://vickikraft.com/"},
+    { district: 3, name: "Lucy Lauser", party: "D", site: "https://ballotpedia.org/Lucy_Lauser"},
+    { district: 3, name: "Matthew Overton", party: "R", site: "https://ballotpedia.org/Matthew_Overton"},
+    { district: 3, name: "Davy Ray", party: "D", site: "https://www.facebook.com/davyrayforcongress"},
+    { district: 3, name: "Heidi St. John", party: "R", site: "https://friendsofnatalie.com/"},
+
+
+
+    { district: 4, name: "Dan Newhouse", party: "R", site: "https://ballotpedia.org/Ted_Cummings"},
+    { district: 4, name: "Loren Culp", party: "R", site: "https://culpforcongress.com/"},
+    { district: 3, name: "Benancio Garicia III", party: "R", site: "https://www.votebengarcia.com/"},
+    { district: 4, name: "Corey Gibson", party: "R", site: "https://www.sendcorey2022.com/" },
+    { district: 4, name: "Brad Klippert", party: "R", site: "https://bradklippert.houserepublicans.wa.gov/"},    
+    { district: 4, name: "Jerrod Sessler", party: "R", site: "https://www.jerrodforcongress.com/"},    
+    { district: 4, name: "Doug White", party: "D", site: "https://www.dougwhite4congress.us/"},
+
+
+    { district: 5, name: "Cathy McMorris Rodgers", party: "R", site: "https://mcmorris.house.gov/"},
+    { district: 5, name: "Sean Clynch", party: "R", site: "https://www.clynchforcongress.com/"},
+    { district: 5, name: "Ann Marie Danimus", party: "D", site: "https://ballotpedia.org/United_States_House_of_Representatives_elections_in_Washington,_2022"},
+    { district: 5, name: "Natasha Hill", party: "D", site: "https://www.natashaforcongress.com/"},
+
+
+
+
+    { district: 6, name: "Derek Kilmer", party: "D", site: "https://kilmer.house.gov/"},
+    { district: 6, name: "Chris Binns", party: "R", site: "https://binnsforthe6th.com/"},
+    { district: 6, name: "Todd Bloom", party: "R", site: "https://www.electtoddbloom.net/"},
+    { district: 6, name: "Elizabeth Kreiselmaier", party: "R", site: "https://cleanupthehouse.com/"},
+    { district: 6, name: "Rebecca Parson", party: "D", site: "https://www.rebeccaparson.com/"},
+
+
+
+    { district: 7, name: "Pramila Jayapal", party: "D", site: "https://jayapal.house.gov/"},
+    { district: 7, name: "Paul Glumaz", party: "R", site: "https://www.glumaz4congress.org/index.html"},
+    { district: 7, name: "Cliff Moon", party: "R", site: "https://vote-usa.org/Intro.aspx?State=WA&Id=WAMoonCliff"},
+
+
+
+    { district: 8, name: "Kim Schrier", party: "D", site: "https://schrier.house.gov/"},
+    { district: 8, name: "Dave Chapman", party: "R", site: "https://ballotpedia.org/Dave_Chapman"},
+    { district: 8, name: "Reagan Dunn", party: "R", site: "https://reagandunn.com/"},
+    { district: 8, name: "Jesse Jensen", party: "R", site: "https://jessejensen4congress.com/"},
+    { district: 8, name: "Matt Larkin", party: "R", site: "https://larkin4congress.com/"},
+    { district: 8, name: "Scott Stephenson", party: "R", site: "https://www.scott4wa8.com/"},
+    { district: 8, name: "Emet Ward", party: "D", site: "https://ballotpedia.org/Washington%27s_8th_Congressional_District_election,_2022"},
+
+
+
+    { district: 9, name: "Adam Smith", party: "D", site: "https://adamsmith.house.gov/"},
+    { district: 9, name: "Douglas Michael Basler", party: "R", site: "https://ballotpedia.org/Doug_Basler"},
+    { district: 9, name: "Sea Chan", party: "R", site: "https://www.chanforcongress.com/"},
+    { district: 9, name: "Stephanie Gallardo", party: "D", site: "https://www.electgallardo.com/"},
+    { district: 9, name: "Seth Pedersen", party: "D", site: "https://ballotpedia.org/Washington%27s_9th_Congressional_District"},
+
+
+
+    { district: 10, name: "Marilyn Strickland", party: "D", site: "https://strickland.house.gov/"},
+    { district: 10, name: "Dan Gordon", party: "R", site: "https://ballotpedia.org/Dan_Gordon_(Washington)"},
+    { district: 10, name: "Eric Mahaffy", party: "D", site: "https://ericmahaffy.com/"},
+    { district: 10, name: "Keith Swank", party: "R", site: "https://keithswank.com/index.html"},
 ]
 
 
 //User Array which will hold Candidate class objects
-let userArray = []
-//needed to make a seperate array to hold checkbox ids as strings not object properties
-//I'm pretty sure there is a way to use include() for object properties but I could
-//not get that to work just yet
+let serverArray = []
 
-let candidateObject = function (cName, cNotes, cID, cParty) {
-    this.testName = cName;
-    this.testNotes = cNotes;
-    this.testID = cID;
-    this.testParty = cParty
-}
+var fs = require("fs");
+
+let fileManager  = {
+  x: 33,
+  read: function() {
+    var rawdata = fs.readFileSync('objectdata.json');
+    let goodData = JSON.parse(rawdata);
+    serverArray = goodData;
+  },
+
+  write: function() {
+    let data = JSON.stringify(serverArray);
+    fs.writeFileSync('objectdata.json', data);
+  },
+
+  validData: function() {
+    var rawdata = fs.readFileSync('objectdata.json');
+    console.log(rawdata.length);
+    if(rawdata.length < 1) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+};
+
+
+
+
 
 
 /* GET home page. */
@@ -42,11 +127,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/getDistrictCandidates', function (req, res) {
-    res.status(200).json(testCandidateArray)
+    res.status(200).json(candidateArray);
+
 })
 
 router.get('/getCandidates', function (req, res) {
-    res.status(200).json(userArray)
+    fileManager.read();
+    res.status(200).json(serverArray);
+    console.log("after file read: " + serverArray)
 })
 
 
@@ -54,24 +142,22 @@ router.get('/getCandidates', function (req, res) {
 
 router.post('/addCandidate', function (req, res) {
     const newCandidate = req.body;
-    userArray.push(newCandidate);
+    serverArray.push(newCandidate);
+    fileManager.write();
     res.status(200).json(newCandidate);
 })
 
-
 router.delete('/deleteCandidate/:testID', function (req, res) {
-    const delID = req.params.testID;
-    let found = false;
-    for (p in userArray){
-        if (delID == userArray[p].testID){
-            const index = userArray.indexOf(userArray[p]);
-            userArray.splice(index, 1)
-        }
-        console.log(delID);
-        console.log(userArray);
-    }
 
-   // userArray.splice(testID, 1)
+    const delID = req.params.testID;
+    console.log(delID);
+    let index = serverArray.findIndex(
+        element => element.testID === delID
+    );
+    serverArray.splice(index, 1);
+    fileManager.write();    
+   // console.log("After splice: "+ serverArray);
+    res.status(200).json(serverArray)
 
 
 })
@@ -79,19 +165,20 @@ router.delete('/deleteCandidate/:testID', function (req, res) {
 router.put('/updateCandidate/:testID', function (req, res)
 {   
     const updateID = req.params.testID;
-    const updateNotes = req.params.testNotes;
+    const updateNotes = req.body.testNotes;
+  
     let found = false
 
-    for (u in userArray){
-        if (updateID == userArray[u].testID){
-            userArray[u].testNotes = updateNotes
+
+
+    for (u in serverArray){
+        if (updateID == serverArray[u].testID){
+            serverArray[u].testNotes = updateNotes;
         }
 
     }
-
-    console.log("testNotes: "+ req.params.testNotes)
-    console.log("testID: "+ req.params.testID);
-    
+    fileManager.write();
+    res.status(200).json(updateNotes);  
 
     }
 )
